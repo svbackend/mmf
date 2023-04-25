f1de3fe510d96a1f6cf9ab73d91222caf795400b
Valentyn Saik <svbackend22@gmail.com>
2023-04-25 02:02:33 -0400

    Assign credits validation fix

--- api/src/User/Http/Request/AssignCreditsRequest.php
+++ api/src/User/Http/Request/AssignCreditsRequest.php
@@ -3,10 +3,12 @@
 namespace App\User\Http\Request;
 
 use App\Common\Http\Request\HttpInputInterface;
+use OpenApi\Annotations as OA;
 use Symfony\Component\Validator\Constraints as Assert;
 
 class AssignCreditsRequest implements HttpInputInterface
 {
+    /** @OA\Property(example="17.50") */
     public float $credits;
 
     public function __construct(float $credits)
@@ -18,6 +20,7 @@ class AssignCreditsRequest implements HttpInputInterface
     {
         return new Assert\Collection([
             'credits' => [
+                new Assert\Type('float'),
                 new Assert\NotBlank(),
                 new Assert\PositiveOrZero(),
             ],
